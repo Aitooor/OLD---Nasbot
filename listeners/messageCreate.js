6a20e6530e1e6bf8679872b2a6d546c344933a04
CayeOficial <lobacaye@gmail.com>
2022-11-01 14:45:08 +0100

    Update messageCreate.js

--- listeners/messageCreate.js
+++ listeners/messageCreate.js
@@ -1,10 +1,10 @@
 const { config } = require("../index.js")
 
 module.exports = async (client, message) => {
-    if(config.sugerenciasEs.channels.some(ch => ch === message.channel.id) && message.author.id !== client.user.id) {
+    if(config.suggestionsEs.channels.some(ch => ch === message.channel.id) && message.author.id !== client.user.id) {
         await client.emit("suggestionCreate", message, "ES");
     }
-    if(config.sugerenciasEn.channels.some(ch => ch === message.channel.id) && message.author.id !== client.user.id) {
+    if(config.suggestionsEn.channels.some(ch => ch === message.channel.id) && message.author.id !== client.user.id) {
         await client.emit("suggestionCreate", message, "EN");
     }
 }
