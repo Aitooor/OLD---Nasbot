6b61a57b5d6416ca43c9df4836b7af03967f205d
CayeOficial <lobacaye@gmail.com>
2023-01-15 19:56:02 +0100

    fix: wrong file

--- listeners/suggestionVoteAdd.js
+++ listeners/suggestionVoteAdd.js
@@ -1,33 +1,39 @@
-const { config, messages } = require("../index.js")
-const { database } = require("../database/database.js")
+const { config } = require("../index.js")
+const { database } = require("../database/database.js");
+const { ActionRowBuilder, ButtonStyle } = require("discord.js");
+const { ButtonBuilder } = require("@discordjs/builders");
 
 module.exports = async (client, reaction, language) => {
 
-    if(reaction.partial) {
-	    try {
-		    await reaction.fetch();
-	    } catch (e) {
-		    return;
-	    }
-    }
+  if(reaction.partial) {
+	  try {
+		  await reaction.fetch();
+	  } catch (e) {
+		  return;
+	  }
+  }
 
-    if(language === "ES") {
-        cnf = {
-            a: config.suggestionsEs.reactions.down,
-            b: config.suggestionsEs.reactions.up,
-            c: config.suggestionsEs.votes_until_review,
-            d: config.suggestionsEs.review_channel
-        }
-    } else {
-        cnf = {
-            a: config.suggestionsEn.reactions.down,
-            b: config.suggestionsEn.reactions.up,
-            c: config.suggestionsEn.votes_until_review,
-            d: config.suggestionsEn.review_channel
-        }
+  if(language === "ES") {
+    cnf = {
+        a: config.sugerenciasEs.reactions.down,
+        b: config.sugerenciasEs.reactions.up,
+        c: config.sugerenciasEs.votes_until_review,
+        d: config.sugerenciasEs.review_channel,
+        e: config.sugerenciasEs.buttonAccept,
+        f: config.sugerenciasEs.buttonDeny
     }
+  } else {
+    cnf = {
+        a: config.sugerenciasEn.reactions.down,
+        b: config.sugerenciasEn.reactions.up,
+        c: config.sugerenciasEn.votes_until_review,
+        d: config.sugerenciasEn.review_channel,
+        e: config.sugerenciasEn.buttonAccept,
+        f: config.sugerenciasEn.buttonDeny
+    }
+  }
 
-    if(!reaction.message.reactions.cache.get(cnf.a)) return;
+  if(!reaction.message.reactions.cache.get(cnf.a)) return;
 
     const votos = (reaction.message.reactions.cache.get(cnf.b).count) + (reaction.message.reactions.cache.get(cnf.a).count);
 
@@ -36,6 +42,19 @@ module.exports = async (client, reaction, language) => {
         await reaction.message.guild.channels.cache.get(cnf.d).send({
             embeds: [
                 reaction.message.embeds[0]
+            ],
+            components: [
+              new ActionRowBuilder()
+              .addComponents(
+                new ButtonBuilder()
+                .setLabel(cnf.e)
+                .setStyle(ButtonStyle.Success)
+                .setCustomId("accept_suggestion"),
+                new ButtonBuilder()
+                .setLabel(cnf.f)
+                .setStyle(ButtonStyle.Danger)
+                .setCustomId("deny_suggestion")
+              )
             ]
         })
         .then(async (message) => {
